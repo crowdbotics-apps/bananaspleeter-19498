c38a6d6e1de9071d4fe711dcebb8ceacde0e6a5c
Crowdbotics <team@crowdbotics.com>
2020-08-11 16:21:45 +0000

    Update models

--- bananaspleeter_19498/wsgi.py
+++ bananaspleeter_19498/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bananaspleeter_19498.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bananaspleeter_19498.settings")
 
 application = get_wsgi_application()
